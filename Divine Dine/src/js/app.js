3fb12a86607b06ee181e0f44a81f106693bb72cf
Pablo Asensi <142694239+AsensiDev@users.noreply.github.com>
2024-04-20 13:22:15 +0200

    front end + scroll efect

--- Divine Dine/src/js/app.js
+++ Divine Dine/src/js/app.js
@@ -1,4 +1,5 @@
-// swiper.js 
+document.addEventListener('DOMContentLoaded', function() {
+    // swiper.js 
 var swiper = new Swiper(".home-slider", { //swiper.js buscará los elementos con esa clase y los convertirá en un carrusel
     loop: true, //bucle infinito 
     effect: "coverflow", //codigo extraído de swiper q hace el efecto al moverse sea distinto
@@ -12,6 +13,31 @@ var swiper = new Swiper(".home-slider", { //swiper.js buscará los elementos con
         slideShadows: false, //si se debe mostrar sombras de las tarjetas
     },
 });
+
+var links = document.querySelectorAll('a[href^="#"]');
+
+    
+    links.forEach(function(link) {
+      link.addEventListener("click", function(e) {
+        e.preventDefault(); 
+
+        var targetId = this.getAttribute("href"); 
+        var targetElement = document.querySelector(targetId); 
+
+        if (targetElement) {
+          
+          var offset = targetElement.offsetTop;
+
+          window.scrollTo({
+            top: offset,
+            behavior: "smooth"
+          });
+        }
+      });
+    });
+});
+
+
 
 
 
