9b2cef1c605264c0686234efcc6431ccd0e28e72
Pablo Asensi <142694239+AsensiDev@users.noreply.github.com>
2024-04-23 20:01:02 +0200

    button upgrade

--- Divine Dine/src/js/app.js
+++ Divine Dine/src/js/app.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     // swiper.js 
-var swiper = new Swiper(".home-slider", { //swiper.js buscará los elementos con esa clase y los convertirá en un carrusel
-    loop: true, //bucle infinito 
-    effect: "coverflow", //codigo extraído de swiper q hace el efecto al moverse sea distinto
+var swiper = new Swiper(".home-slider", { 
+    loop: true, 
+    effect: "coverflow", 
     spaceBetween: 30,
-    grabCursor: true, //cambia el curso a una mano
+    grabCursor: true, 
     coverflowEffect: {
-        rotate: 50, //rotación de las tarjeta
-        stretch: 0, //extensión de las tarjetas
-        depth: 100, //profundidad de las tarjetas
-        modifier: 1, //velocidad de transición de las tarjetas
-        slideShadows: false, //si se debe mostrar sombras de las tarjetas
+        rotate: 50, 
+        stretch: 0, 
+        depth: 100, 
+        modifier: 1, 
+        slideShadows: false, 
     },
 });
 
